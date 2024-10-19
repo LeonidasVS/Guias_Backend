c5b78179825c542cbd181d1d78fee9c47b11c3cc
LeonidasVS <villafrancosibrian@gmail.com>
2024-10-19 14:27:58 -0600

    Guia 3, 4 y 5. Creacion del Proyecto, controladores y Metodos

---
+++ Backend/Controllers/OperationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(decimal a, decimal c)
+        {
+            return a + c;   
+        }
+
+        [HttpPost]
+        public decimal add(decimal a, decimal c)
+        {
+            return a - c;
+        }
+
+        [HttpPut]
+        public decimal multiplicar(decimal a, decimal c)
+        {
+            return a * c;
+        }
+
+        [HttpDelete]
+        public decimal Nmas1(decimal a, decimal c)
+        {
+            return a * (a * c);
+        }
+    }
+}
